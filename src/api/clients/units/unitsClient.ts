e4ac33afc970199e5bae0850ab674ff44e742465
Frank Rojas <fco3190@gmail.com>
2025-03-06 11:57:22 -0500

    Cambios del Client

--- src/api/clients/units/unitsClient.ts
+++ src/api/clients/units/unitsClient.ts
@@ -1,17 +1,37 @@
-import { ConnectionAPI } from "@/api/connectionAPI"
-import { GenericRequest } from "@/api/genericRequest"
+import { ConnectionAPI } from '@/api/connectionAPI'
+import { GenericRequest } from '@/api/genericRequest'
 
 const url = ConnectionAPI()
 const base = url + '/Unidades'
 
-export const GetUnits = async() =>{
-  return await GenericRequest({ url: base, method: "GET" });
+export const GetUnits = async () => {
+  return await GenericRequest({ url: base, method: 'GET' })
 }
 
-export const  PostUnits = async(values) => {
-  return await GenericRequest({ url: base, method: "POST", data: {
-    nombre: values.nombre,
-    descripcion: values.descripcion,
-    esBorrado: false
-  }});
+export const PostUnits = async (values) => {
+  return await GenericRequest({
+    url: base,
+    method: 'POST',
+    data: {
+      nombre: values.nombre,
+      descripcion: values.descripcion,
+      esBorrado: false,
+    },
+  })
+}
+
+export const UpdateUnits = async (values) => {
+  return await GenericRequest({
+    url: base,
+    method: 'PUT',
+    data: {
+      nombre: values.nombre,
+      descripcion: values.descripcion,
+      esBorrado: values.esBorrado,
+    },
+  })
+}
+
+export const DeleteUnit = async (id: number) => {
+  return await GenericRequest({ url: `${base}/${id}`, method: 'DELETE' })
 }
